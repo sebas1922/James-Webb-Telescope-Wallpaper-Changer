128398f0945bb086286b3dd20c2ce23520235e49
sebas1922 <sebastian.gonzalez1922@gmail.com>
2024-09-01 14:10:51 -0400

    Got it to fetch random image and save it to folder

--- JWST_wp_script.py
+++ JWST_wp_script.py
@@ -1,48 +1,36 @@
 import requests
 import json
 import argparse
-import random
+from random import choice
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os 
 
-api_key = os.getenv("FLICKR_API_KEY")
+def main():
+    load_dotenv()
 
-ALBUM_IDS = [{"year": 2024, "Id": "72177720313923911"}, {"year":2023, "Id":"72177720305127361"}, {"year":2022, "Id": "72177720305127361"}]
-url = "https://www.flickr.com/photos/nasawebbtelescope/albums/"+random.choice(ALBUM_IDS)["Id"]
+    api_key = os.getenv("FLICKR_API_KEY")
 
-payload = {}
-headers = {}
+    ALBUM_IDS = [{"year": 2024, "Id": "72177720313923911"}, {"year":2023, "Id":"72177720305127361"}, {"year":2022, "Id": "72177720305127361"}]
 
+    url = "https://api.flickr.com/services/rest/"
 
-response = requests.get(url, headers=headers, data=payload)
-
-print(response)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-print(soup.find("div", {"class": "view album-page-view flickr-view-root-view"}))
+    payload = {"api_key": api_key}
+    headers = {}
 
 
-"""
-First get the id of each image from the response and put into a list
-"""
+    #response = requests.get(url+"method=flickr.photosets.getList", headers=headers, params=payload)
 
-#print(json.dumps(photo_data, indent=4))
-
-image_id = []
-for image in photo_data:
-    image_id.append(image["id"])   
+    print(get_user_id(url, payload))
 
 
-"""
-Next we need to get the url of each image from the id and through there we can download the image itself
-"""
 
 
-for Id in image_id:
-    url = f"https://www.flickr.com/photos/nasawebbtelescope/"+Id
-    print(requests.get(url).content)
-    
+def get_user_id(url, payload):
+    r = requests.get(url+"method=flickr.people.findByUsername", 
+        params= payload | {"username" : "Flickr"})
+    return r.content
 
- 
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # James-Webb-Telescope-Wallpaper-Changer
 Web Scraping sript that takes gallery images from flickr.com uploaded by NASA taken by the JWST and sets an image to the desktop wallpaper.
+
+
